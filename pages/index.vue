13ce60b27336b3ce4a4e116cf16f2689cd45a36b
Jose Manuel Piñeiro Garcia <josempga@ext.inditex.com>
2022-02-18 15:25:24 +0000

    Add link to pokemon

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,18 @@
 <template>
   <div :class="classes">
-    <h1>Index</h1>
+    <h1 @click="goPokedex">Index</h1>
   </div>
 </template>
 
 <script setup>
+const router = useRouter()
+const goPokedex = () => {
+  router.push({
+    name: 'pokemon-id',
+    params: { id: '1' },
+  })
+}
 </script>
 
-<style lang="scss">
+<style lang="css">
 </style>
