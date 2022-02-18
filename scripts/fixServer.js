adb323970c266e60624e24418675c8051ca5653b
Jose Manuel Piñeiro Garcia <josempga@ext.inditex.com>
2022-02-18 19:02:06 +0000

    Update fix server file path

--- scripts/fixServer.js
+++ scripts/fixServer.js
@@ -1,7 +1,7 @@
 const replace = require('replace-in-file')
 
 const options = {
-  files: '.vercel_build_output/server/chunks/app/server.mjs',
+  files: '.vercel_build_output/functions/node/server/chunks/app/server.mjs',
   from: /@apollo\/client\/core/g,
   to: '@apollo/client/core/core.cjs',
 }
