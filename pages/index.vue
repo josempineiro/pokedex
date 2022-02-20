66b324da2437c3808d585429f5ff4fc13183cb03
Jose Manuel Piñeiro Garcia <josempga@ext.inditex.com>
2022-02-20 22:35:06 +0000

    Add beep and navigation with click

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="HomePage">
-    <NuxtLink to="/pokemon/1"> <PokedexIcon /></NuxtLink>
-    <audio ref="beep">
+  <div class="HomePage" id="HomePage">
+    <button class="StartButton" @click="playBeep">
+      <PokedexIcon @click="playBeep" :class="{ started }" />
+    </button>
+    <audio ref="beep" @ended="ended">
       <source src="~/assets/audio/beep.mp3" type="audio/mp3" />
     </audio>
   </div>
@@ -24,44 +26,64 @@ definePageMeta({
 
 const router = useRouter()
 const beep = ref(null)
-onMounted(() => {
-  setTimeout(function () {
-    beep.value.play()
-  }, 1000)
-  setTimeout(function () {
-    router.push({
-      name: 'pokemon-id',
-      params: { id: '1' },
-    })
-  }, 3000)
-})
+const started = ref(false)
+
+function playBeep() {
+  return beep.value.play()
+}
+
+function navigateToPokedex() {
+  router.push({
+    name: 'pokemon-id',
+    params: { id: '1' },
+  })
+}
+
+function start() {
+  started.value = true
+  playBeep()
+}
+
+function ended() {
+  setTimeout(navigateToPokedex, 500)
+}
 </script>
 
 <style lang="scss">
 @keyframes spin {
   0% {
     filter: drop-shadow(0px 0px 0px rgba(0, 0, 0, 0.3));
-    opacity: 0;
+    transform: scale(1);
   }
-  25% {
+  2% {
     transform: scale(1.1);
     filter: drop-shadow(0px 0px 5px rgba(0, 0, 0, 0.3));
-    opacity: 1;
   }
-  40% {
+  4% {
     transform: scale(1.1) rotate(15deg);
   }
-  60% {
+  6% {
     transform: scale(1.1) rotate(-15deg);
   }
-  75% {
+  8% {
     transform: scale(1.1);
     filter: drop-shadow(0px 0px 5px rgba(0, 0, 0, 0.3));
-    opacity: 1;
+  }
+  10% {
+    filter: drop-shadow(0px 0px 0px rgba(0, 0, 0, 0.3));
+    transform: scale(1);
+  }
+}
+
+@keyframes active {
+  33% {
+    transform: scale(1.1) rotate(-15deg);
+  }
+  66% {
+    transform: scale(1.1) rotate(15deg);
   }
   100% {
-    filter: drop-shadow(0px 0px 0px rgba(0, 0, 0, 0.3));
-    opacity: 0;
+    transform: scale(1) rotate(0);
   }
 }
 .HomePage {
@@ -70,12 +92,37 @@ onMounted(() => {
   align-items: center;
   height: 100%;
   width: 100%;
-  .PokedexIcon {
-    animation-name: spin;
-    animation-duration: 2s;
-    animation-iteration-count: 1;
+
+  .StartButton {
+    border: none;
+    padding: none;
+    outline: none;
+    background: none;
+    cursor: pointer;
+    transition-property: transform, filter;
+    transition-timing-function: ease;
+    transition-duration: 1s;
+    animation-duration: 10s;
+    animation-iteration-count: infinite;
     animation-delay: 1s;
-    opacity: 0;
+    animation-fill-mode: forwards;
+    animation-timing-function: ease;
+  }
+  .StartButton:not(:hover) {
+    animation-name: spin;
+  }
+
+  .StartButton:hover:not(:focus) {
+    animation-name: none;
+    transform: scale(1.1);
+    filter: drop-shadow(0px 0px 5px rgba(0, 0, 0, 0.3));
+  }
+
+  .StartButton:focus {
+    animation-delay: 0s;
+    animation-name: active;
+    animation-iteration-count: 1;
+    animation-duration: 1s;
   }
 }
 
