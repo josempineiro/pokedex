6369f989a650a57c92a55540dd02ee7d038491c4
Jose Manuel Piñeiro Garcia <josempga@ext.inditex.com>
2022-02-20 21:26:38 +0000

    Improve manifest and meta tags

--- app.vue
+++ app.vue
@@ -2,18 +2,20 @@
   <Html>
     <Head>
       <Title>Awesome Pokedex App</Title>
+      <Meta charset="utf-8" />
       <Meta
         name="description"
         content="Awesome Pokédex App built with Nuxt3 and free PokeAPI"
       />
       <Meta name="viewport" content="width=device-width, initial-scale=1" />
-      <Meta name="msapplication-TileColor" content="var(--color-primary-rgb)" />
-      <Meta
-        name="msapplication-TileImage"
-        content="icons//ms-icon-144x144.png"
-      />
+      <Meta name="msapplication-TileColor" content="#d1092b" />
+      <Meta name="msapplication-TileImage" content="/icons/Pokedex.svg" />
       <Meta name="theme-color" content="#d1092b" />
       <Link rel="manifest" href="/manifest.json" />
+      <Meta
+        name="google-site-verification"
+        content="12SzhX8l1tk3jBwLdqfOAbsJ-5DaB-AWcHKd7nJICI8"
+      />
     </Head>
     <Body>
       <NuxtLayout>
