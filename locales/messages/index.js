e49188df7d8709f0ca09faa6d671954a889b928b
Jose Manuel Piñeiro Garcia <josempga@ext.inditex.com>
2022-02-21 23:29:47 +0000

    Remove unused code

--- locales/messages/index.js
+++
@@ -1,3 +0,0 @@
-// @index('./**/*.json', f => `export { default as ${f.name} } from '${f.path}'`)
-export { default as en } from './en'
-export { default as es } from './es'
--- nuxt.config.js
+++ nuxt.config.js
@@ -2,10 +2,6 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  meta: {
-    script: [
-      { src: 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js' },
-    ],
-  },
+  meta: {},
   css: ['~/assets/styles/app.scss'],
 })
