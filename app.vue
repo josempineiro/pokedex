df3f6e753b4eadcec8fb5dd0d77a6034fa38946d
Jose Manuel Piñeiro Garcia <josempga@ext.inditex.com>
2022-02-21 10:08:36 +0000

    Improve meta tags

--- app.vue
+++ app.vue
@@ -3,6 +3,8 @@
     <Head>
       <Title>Awesome Pokedex App</Title>
       <Meta charset="utf-8" />
+
+      <meta name="title" content="Awesome Pokedex App" />
       <Meta
         name="description"
         content="Awesome Pokédex App built with Nuxt3 and free PokeAPI"
@@ -15,6 +17,32 @@
       <Meta
         name="google-site-verification"
         content="12SzhX8l1tk3jBwLdqfOAbsJ-5DaB-AWcHKd7nJICI8"
+      />
+
+      <!-- Open Graph / Facebook -->
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://pokedex.leman.dev/" />
+      <meta property="og:title" content="Awesome Pokedex App" />
+      <meta
+        property="og:description"
+        content="Awesome Pokédex App built with Nuxt3 and free PokeAPI"
+      />
+      <meta
+        property="og:image"
+        content="https://pokedex.leman.dev/images/preview.png"
+      />
+
+      <!-- Twitter -->
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://pokedex.leman.dev/" />
+      <meta property="twitter:title" content="Awesome Pokedex App" />
+      <meta
+        property="twitter:description"
+        content="Awesome Pokédex App built with Nuxt3 and free PokeAPI"
+      />
+      <meta
+        property="twitter:image"
+        content="https://pokedex.leman.dev/images/preview.png"
       />
     </Head>
     <Body>
